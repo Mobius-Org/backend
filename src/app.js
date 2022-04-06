8739ccd8ff8a4e22b2a0cd14901e2c02e9585a27
Nweke Chidimma Dominic <realchidinweke@gmail.com>
2022-04-06 10:51:20 +0100

    chore: File setup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,37 @@
-const express = require('express');
+const cors                 = require('cors');
+const morgan               = require('morgan');
+const express              = require('express');
+const AppError             = require('./errors/appError');
+const { cloudinaryConfig } = require('./utils/cloudinary');
+const appErrorHandler      = require('./errors/app_error_handler');
+
+// Initalize app
 const app = express();
+
+//Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// enable morgan
+app.use(morgan('dev'));
+
+// config cloudinary
+app.use('*', cloudinaryConfig);
+
+//ROUTES
+
+
+app.get('/', (req, res) =>{
+  res.status(200).send("Welcome To The Home Page Of MOBIUS Api!")
+});
+
+
+
+app.all('**', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+
+app.use(appErrorHandler);
 
 module.exports = app;
