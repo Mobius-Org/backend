d3a3d26a2cdf4cdcbe1fe7b27e9ef66bfbd7886b
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-04-14 16:32:41 +0100

    chore: update base url

--- src/controllers/googleController.js
+++ src/controllers/googleController.js
@@ -8,7 +8,7 @@ const AppError = require('../errors/appError');
 const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-   "http://localhost:3002/mbApi/v1/google/auth/callback"
+   "https://mobius-stage.herokuapp.com/mbApi/v1/google/auth/callback"
 )
 
 // `${process.env.TREEN_SERVER}/${redirectURI}`,
