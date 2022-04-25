2ae35a403f28c22d11c318a15e42949a7a0eb177
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-04-25 08:24:16 +0100

    edit: edit enroll course endpoint

--- src/routes/courseRoute.js
+++ src/routes/courseRoute.js
@@ -21,7 +21,7 @@ router.get("/all", courseController.getAllCourses);
 //get one course
 router.get("/:id", courseController.getOneCourse);
 //post user being enrolled     /first id is the course in question while the second id is tht if the user being enrolled
-router.patch("/:id", auth, courseController.enrollCourse);
+router.patch("/enroll/:id", auth, courseController.enrollCourse);
 
 // upload content video :id ==> id of the course in question
 router.post(
